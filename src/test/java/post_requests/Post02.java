09a9765191a18df6ae39164257a1a95eba0d6cb4
sedat <somamebarge@gmail.com>
2023-10-11 22:19:37 +0200

    API JsonPath, Groovy Language, Serialization

--- src/test/java/post_requests/Post02.java
+++ src/test/java/post_requests/Post02.java
@@ -1,17 +1,13 @@
 package post_requests;
-
 import baseUrl.JsonPlaceHolderBaseUrl;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-
 public class Post02 extends JsonPlaceHolderBaseUrl {
-
-          /*
+    /*
     Given
        1) https://jsonplaceholder.typicode.com/todos
        2)  {
@@ -31,32 +27,31 @@ public class Post02 extends JsonPlaceHolderBaseUrl {
                 "completed": false,
                 "id": 201
             }
-           */
-
+     */
     @Test
-    public void post02() {
-
+    public void post02JsonPath() {
         // Set the URL
         spec.pathParam("first", "todos");
-
         // Set the expected data (payLoad)
-        // PayLoad: Post işlemlerinde Body ile gönderdiğimiz datadır.
-        // Expected Data: Response'tan gelmesini beklediğimiz datadır.
-        String payLoad = "{ \n" +
-                "             \"userId\": 55, \n" +
-                "             \"title\": \"Tidy your room\", \n" +
-                "             \"completed\": false \n" +
+        // Payload: Body içerisinde POST Request ile gönderdiğimiz data
+        // Expected Data: Request sonucunda response'tan gelmesini beklediğimiz data
+        String payLoad = "{\n" +
+                "             \"userId\": 55,\n" +
+                "             \"title\": \"Tidy your room\",\n" +
+                "             \"completed\": false\n" +
                 "           }";
-
         // Send the request and get the response
         Response response = given(spec).body(payLoad).when().post("{first}");
         response.prettyPrint();
-
         // Do assertion
-        JsonPath json = response.jsonPath();
         assertEquals(201, response.statusCode());
+        JsonPath json = response.jsonPath();
         assertEquals(55, json.getInt("userId"));
         assertEquals("Tidy your room", json.getString("title"));
         assertFalse(json.getBoolean("completed"));
+    }
+    @Test
+    public void post02Map() {
+
     }
 }
